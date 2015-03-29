e1ef9f25dce113d82cd90d8c42e481c5a63ad527
[ravencovington] <rcov00@gmail.com>
2015-03-29 15:06:25 -0400

    Fixed controllers

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -1,5 +1,4 @@
 class AgenciesController < ApplicationController
-  before_action :set_agency, only: [:show, :edit, :update, :destroy]
 
   # GET /agencies
   def index
@@ -8,41 +7,11 @@ class AgenciesController < ApplicationController
 
   # GET /agencies/1
   def show
-  end
-
-  # GET /agencies/new
-  def new
-    @agency = Agency.new
-  end
-
-  # GET /agencies/1/edit
-  def edit
-  end
-
-  # POST /agencies
- 
-  def create
-    @agency = Agency.new(agency_params)
-
-  
-  end
-
-  # PATCH/PUT /agencies/1
-  def update
-    
-  end
-
-  # DELETE /agencies/1
-  def destroy
-    @agency.destroy
-  
+    @active = 'agencies'
+    @agency = Agency.find(param[:id.to_i])
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_agency
-      @agency = Agency.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def agency_params
--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,5 +1,5 @@
 class ChildrenController < ApplicationController
-  before_action :set_child, only: [:show, :edit, :update, :destroy]
+  # before_action :set_child, only: [:show, :edit, :update, :destroy]
 
   # GET /children
   def index
@@ -8,38 +8,11 @@ class ChildrenController < ApplicationController
 
   # GET /children/1
   def show
-  end
-
-  # GET /children/new
-  def new
-    @child = Child.new
-  end
-
-  # GET /children/1/edit
-  def edit
-  end
-
-  # POST /children
-  def create
-    @child = Child.new(child_params)
-  end
-
-  # PATCH/PUT /children/1
-  # PATCH/PUT /children/1.json
-  def update
-  end
-
-  # DELETE /children/1
-  # DELETE /children/1.json
-  def destroy
-    @child.destroy
+    @active = 'children'
+    @child  = Child.find(param[:id].to_i)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_child
-      @child = Child.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def child_params
