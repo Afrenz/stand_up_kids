87ff1c623892d7ea1007dc2513f5c308e9de0a83
[ravencovington] <rcov00@gmail.com>
2015-03-29 12:37:25 -0400

    reduced fluff

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -2,13 +2,11 @@ class AgenciesController < ApplicationController
   before_action :set_agency, only: [:show, :edit, :update, :destroy]
 
   # GET /agencies
-  # GET /agencies.json
   def index
     @agencies = Agency.all
   end
 
   # GET /agencies/1
-  # GET /agencies/1.json
   def show
   end
 
@@ -22,43 +20,22 @@ class AgenciesController < ApplicationController
   end
 
   # POST /agencies
-  # POST /agencies.json
+ 
   def create
     @agency = Agency.new(agency_params)
 
-    respond_to do |format|
-      if @agency.save
-        format.html { redirect_to @agency, notice: 'Agency was successfully created.' }
-        format.json { render :show, status: :created, location: @agency }
-      else
-        format.html { render :new }
-        format.json { render json: @agency.errors, status: :unprocessable_entity }
-      end
-    end
+  
   end
 
   # PATCH/PUT /agencies/1
-  # PATCH/PUT /agencies/1.json
   def update
-    respond_to do |format|
-      if @agency.update(agency_params)
-        format.html { redirect_to @agency, notice: 'Agency was successfully updated.' }
-        format.json { render :show, status: :ok, location: @agency }
-      else
-        format.html { render :edit }
-        format.json { render json: @agency.errors, status: :unprocessable_entity }
-      end
-    end
+    
   end
 
   # DELETE /agencies/1
-  # DELETE /agencies/1.json
   def destroy
     @agency.destroy
-    respond_to do |format|
-      format.html { redirect_to agencies_url, notice: 'Agency was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  
   end
 
   private
